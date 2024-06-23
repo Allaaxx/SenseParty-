49b691288a9e2a088313cb075039ab0e2d8ba49c
Allaaxx <allanrao358@gmail.com>
2024-06-23 09:52:00 -0300

    Nav Remasterizado

--- resources/views/front/layout/inc/header.blade.php
+++ resources/views/front/layout/inc/header.blade.php
@@ -7,7 +7,8 @@
           <!-- logo -->
           <div class="site-logo">
             <a href="{{ route('home-page') }}">
-              <img style="max-height: 64px;" src="/images/site/{{ get_settings()->site_favicon }}" alt="Site Logo">
+              <img style="max-height: 64px;" src="/images/site/{{ get_settings()->site_favicon }}"
+                alt="Site Logo">
             </a>
           </div>
           <!-- logo -->
@@ -15,7 +16,8 @@
           <!-- menu start -->
           <nav class="main-menu">
             <ul>
-              <li class="current-list-item"><a href="{{ route('home-page') }}">Início</a></li>
+              <li><a href="{{ route('home-page') }}">Início</a></li>
+
               <li>
                 <a href="#">Categorias</a>
                 @if (count(get_categories()) > 0)
@@ -23,7 +25,8 @@
                 <ul class="sub-menu">
                   <li class="hover-sub">
                     <a href="#" class="categoria">
-                      <img src="/images/categories/{{ $category->category_image }}" alt="{{ $category->category_name }}">
+                      <img src="/images/categories/{{ $category->category_image }}"
+                        alt="{{ $category->category_name }}">
                       <h6>{{ $category->category_name }}</h6>
                       @if (count($category->subcategories) > 0)
                       <i class="fa fa-angle-down"></i>
@@ -32,12 +35,15 @@
                           @foreach ($category->subcategories as $subcategory)
                           @if ($subcategory->is_child_of == 0)
                           <li class="menu-item">
-                            <a href="#"><h6>{{ $subcategory->subcategory_name }}</h6></a>
+                            <a href="#">
+                              <h6>{{ $subcategory->subcategory_name }}</h6>
+                            </a>
                             @if (count($subcategory->children) > 0)
                             <ul>
                               @foreach ($subcategory->children as $child_subcategory)
                               <li class="menu-item">
-                                <a href="javascript:void(0)">{{ $child_subcategory->subcategory_name }}</a>
+                                <a href="javascript:void(0)">{{
+                                  $child_subcategory->subcategory_name }}</a>
                               </li>
                               @endforeach
                             </ul>
@@ -54,24 +60,20 @@
                 @endforeach
                 @endif
               </li>
+
               <li><a href="{{ route('contact-page') }}">Contato</a></li>
               <li><a href="{{ route('shop-page') }}">Loja</a></li>
+              </li>
               <li>
                 <div class="header-icons">
-                  <!-- Ícone do Carrinho com Contador -->
                   @livewire('cart-counter')
-                  
                   <a class="mobile-hide search-bar-icon" href="#"><i class="fas fa-search"></i></a>
-                  
-                  <a class="mobile-show search-bar-icon" href=""><i class="fas fa-user"></i></a>
+                  <a class="mobile-hide search-bar-icon" href="#"><i class="fas fa-user"></i></a>
                 </div>
               </li>
             </ul>
           </nav>
-          
-         
-         
-          
+          <a class="mobile-show search-bar-icon" href="#"><i class="fas fa-search"></i></a>
           <div class="mobile-menu"></div>
           <!-- menu end -->
         </div>
